0560dc66d90ae8aab493c40111adec4b85cd56ed
Massoud114 <massoudfataou@gmail.com>
2021-05-28 14:52:24 +0100

    Animate some components
    Signed-off-by: Massoud114 <massoudfataou@gmail.com>

--- js/app.js
+++ js/app.js
@@ -58,7 +58,7 @@
     }); 
 
     $(".back-to-top").on("click", function() {
-        $("html, body").animate({ scrollTop: 0 }, 3000);
+        $("html, body").animate({ scrollTop: 0 }, 1000);
         return false;
     });
 }(jQuery)
