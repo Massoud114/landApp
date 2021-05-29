a6f5a12acdbcc9b83092fa3566e14895e8b16a88
Massoud114 <massoudfataou@gmail.com>
2021-05-29 10:12:24 +0100

    Latest Changes
    Signed-off-by: Massoud114 <massoudfataou@gmail.com>

---
+++ js/index.js
@@ -0,0 +1,35 @@
+var jquery = require("jquery");
+window.$ = window.jQuery = jquery;
+
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import 'magnific-popup'
+import 'owl.carousel'
+import 'swiper'
+import 'aos'
+import './scrollspy.min'
+import './plugin.init'
+import './contact'
+import './app'
+
+/* js import */
+/*import './jquery.easing.min'
+import './counter.init'
+import './jquery.nicescroll'
+import './plugin.init'
+import './app'*/
+
+/* css imports */
+import 'magnific-popup/dist/magnific-popup.css'
+import '../css/materialdesignicons.min.css'
+import '../css/pe-icon-7-stroke.css'
+import 'owl.carousel/dist/assets/owl.carousel.min.css'
+import 'owl.carousel/dist/assets/owl.theme.default.css'
+import '../css/owl.transitions.css'
+import 'aos/dist/aos.css'
+import 'swiper/swiper.min.css'
+/*import '../css/owl.carousel.min.css'
+import '../css/owl.theme.css'
+import '../css/style.css'*/
+/**/
